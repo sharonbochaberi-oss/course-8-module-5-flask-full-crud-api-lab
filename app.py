ddea3747d16f88e65b0bd140d760801d7b578380
sharonbochaberi-oss <sharonbochaberi@gmail.com>
2026-05-09 12:18:50 +0300

    Add POST, PATCH, and DELETE routes for events

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ class Event:
 
     def to_dict(self):
         return {
-            "id": self.id, 
+            "id": self.id,
             "title": self.title
         }
 
@@ -20,58 +20,59 @@ events = [
     Event(2, "Python Workshop")
 ]
 
-# Root Route
+
 @app.route("/")
 def home():
-    return jsonify({"message": 'Welcome to the Event API!'})
+    return jsonify({"message": "Welcome to the Event API!"})
 
-# GET ALL events
+
+# GET ALL EVENTS
 @app.route("/events", methods=["GET"])
 def get_events():
     return jsonify([event.to_dict() for event in events])
 
-# CREATE (POST)
+
+# CREATE EVENT
 @app.route("/events", methods=["POST"])
 def create_event():
     data = request.get_json()
 
-    #validate input
     if not data or "title" not in data:
         return jsonify({"error": "Title is required"}), 400
-    
+
     new_id = max(event.id for event in events) + 1 if events else 1
+
     new_event = Event(new_id, data["title"])
     events.append(new_event)
-
 
     return jsonify(new_event.to_dict()), 201
 
 
-#UPDATE (PATCH)
-@app.route("/events/<int:event_id>", methods=["PATCH","DELETE"])
+# UPDATE EVENT
+@app.route("/events/<int:event_id>", methods=["PATCH"])
 def update_event(event_id):
     data = request.get_json()
 
     for event in events:
         if event.id == event_id:
-            if "title" in data:
+            if data and "title" in data:
                 event.title = data["title"]
-            return jsonify(event.to_dict())
+
+            return jsonify(event.to_dict()), 200
 
     return jsonify({"error": "Event not found"}), 404
-    
 
-# DELETE
+
+# DELETE EVENT
 @app.route("/events/<int:event_id>", methods=["DELETE"])
 def delete_event(event_id):
-    global events
-
     for event in events:
         if event.id == event_id:
             events.remove(event)
             return "", 204
-        
+
     return jsonify({"error": "Event not found"}), 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
